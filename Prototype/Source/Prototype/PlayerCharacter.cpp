13835f202c590020c689818a4d95a4dba20be69b
Philipp Welsch <pwelsch86@gmail.com>
2016-12-16 20:50:55 +0100

    added custom Character

---
+++ Prototype/Source/Prototype/PlayerCharacter.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Prototype.h"
+#include "PlayerCharacter.h"
+
+//void PlayerCharacter::SetupInputBindings(UInputComponent* playerInputComponent)
+//{
+//
+//}
+
+
