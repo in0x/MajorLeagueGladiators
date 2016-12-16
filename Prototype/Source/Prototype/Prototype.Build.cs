9fc00ecf5167f0c602d893d6e274678c4971a8a2
Philipp Welsch <pwelsch86@gmail.com>
2016-12-16 20:50:29 +0100

    added VRExpansion source

--- Prototype/Source/Prototype/Prototype.Build.cs
+++ Prototype/Source/Prototype/Prototype.Build.cs
@@ -1,21 +1,89 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class Prototype : ModuleRules
 {
-	public Prototype(TargetInfo Target)
+    public Prototype(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+         PublicIncludePaths.AddRange(
+			new string[] {
+                "HeadMountedDisplay/Public",
+                "Runtime/Engine/Classes/Kismet",
+                "Runtime/Engine/Private/PhysicsEngine"
+			}
+		);
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicDependencyModuleNames.AddRange(
+            new string[] 
+            {
+                "Core",
+                "CoreUObject",
+                "Engine",
+                "InputCore",
+                "PhysX",
+                "HeadMountedDisplay",
+                "RHI",
+                "RenderCore",
+                "ShaderCore",
+                "NetworkReplayStreaming",
+                "AIModule",
+                "ProceduralMeshComponent"
+            });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore"
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+
+
+
+        // Don't load APEX on incompatible platforms
+        if (
+            Target.Platform != UnrealTargetPlatform.IOS &&
+            Target.Platform != UnrealTargetPlatform.TVOS &&
+            Target.Platform != UnrealTargetPlatform.Android &&
+            Target.Platform != UnrealTargetPlatform.HTML5)
+        {
+            PublicDependencyModuleNames.AddRange(
+            new string[]
+            {                   
+             "APEX"
+            });
+        }
+
+
+        // Locking steamVR out of non windows builds
+        if (Target.Platform == UnrealTargetPlatform.Win32 || Target.Platform == UnrealTargetPlatform.Win64)
+        {
+
+            PublicIncludePaths.AddRange(
+            new string[] {
+                        "../Plugins/Runtime/Steam/SteamVR/Source/SteamVR/Private" // This is dumb but it isn't very open
+
+                // ... add public include paths required here ...
+            }
+            );
+
+
+            PublicDependencyModuleNames.AddRange(
+                new string[]
+                {
+                "SteamVR",
+                "OpenVR",
+                "SteamVRController"
+                });
+
+            // AddEngineThirdPartyPrivateStaticDependencies(Target, "OpenVR");
+            //  PrivateDependencyModuleNames.AddRange(new string[] { "D3D11RHI" });     //@todo steamvr: multiplatform
+        }
 	}
 }
--- Prototype/Source/Prototype/Prototype.h
+++ Prototype/Source/Prototype/Prototype.h
@@ -4,3 +4,18 @@
 
 #include "Engine.h"
 
+#include "VRExpansion/VRBPDatatypes.h"
+#include "VRExpansion/GripMotionControllerComponent.h"
+#include "VRExpansion/VRExpansionFunctionLibrary.h"
+#include "VRExpansion/ReplicatedVRCameraComponent.h"
+#include "VRExpansion/ParentRelativeAttachmentComponent.h"
+#include "VRExpansion/VRRootComponent.h"
+#include "VRExpansion/VRCharacterMovementComponent.h"
+#include "VRExpansion/VRCharacter.h"
+#include "VRExpansion/VRPathFollowingComponent.h"
+#include "VRExpansion/VRPlayerController.h"
+#include "VRExpansion/VRGripInterface.h"
+#include "Runtime/Launch/Resources/Version.h"
+#include "PhysXIncludes.h"
+#include "Runtime/Engine/Private/EnginePrivate.h"
+#include "Runtime/Engine/Classes/Kismet/KismetMathLibrary.h"
