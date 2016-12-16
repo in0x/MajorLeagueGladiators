eaa14dd2b1b131dcf98b727d8296158780da169f
Philipp Welsch <pwelsch86@gmail.com>
2016-12-16 21:45:29 +0100

    wrapup commit

--- Prototype/Source/Prototype/PlayerCharacter.h
+++ Prototype/Source/Prototype/PlayerCharacter.h
@@ -6,14 +6,14 @@
 #include "PlayerCharacter.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class PROTOTYPE_API APlayerCharacter : public AVRSimpleCharacter
 {
 	GENERATED_BODY()
+public:
+	APlayerCharacter(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 
-	//virtual void SetupPlayerInputComponent(UInputComponent* playerInputComponent) override;
-	
-	
+	virtual void SetupPlayerInputComponent(UInputComponent* playerInputComponent) override;
 };
