d66b8be1e33f5a9b933c0111ad8e9ef05f1eb464
Philipp Welsch <pwelsch86@gmail.com>
2016-12-16 20:50:55 +0100

    added custom Character

---
+++ Prototype/Source/Prototype/PlayerCharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "VRExpansion/VRSimpleCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPE_API APlayerCharacter : public AVRSimpleCharacter
+{
+	GENERATED_BODY()
+
+	//virtual void SetupPlayerInputComponent(UInputComponent* playerInputComponent) override;
+	
+	
+};
