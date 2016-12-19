4aa216c9848848b21441f42b57ac7fd4a9b0dc01
Philipp Welsch <pwelsch86@gmail.com>
2016-12-19 20:44:29 +0100

    enable mock motion controller for use without HMD

---
+++ Prototype/Source/Prototype/HandComponent.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Prototype.h"
+#include "HandComponent.h"
+
+
+// Sets default values for this component's properties
+UHandComponent::UHandComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+// Called when the game starts
+void UHandComponent::BeginPlay()
+{
+	Super::BeginPlay();	
+}
+
+// Called every frame
+void UHandComponent::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+}
+
+void UHandComponent::SetControllerHand(EControllerHand newHand)
+{
+	hand = newHand;
+
+	FVector offset(75, 25, 0);
+	offset.Y *= hand == EControllerHand::Right ? -1.f : 1.f;
+	AddLocalOffset(offset);
+}
+
--- Prototype/Source/Prototype/PlayerCharacter.cpp
+++ Prototype/Source/Prototype/PlayerCharacter.cpp
@@ -2,19 +2,80 @@
 
 #include "Prototype.h"
 #include "PlayerCharacter.h"
+#include "HandComponent.h"
 
 APlayerCharacter::APlayerCharacter(const FObjectInitializer& ObjectInitializer /*= FObjectInitializer::Get()*/)
 	: Super(ObjectInitializer)
 {
 	bUseControllerRotationPitch = true;
+
+	leftMesh = ObjectInitializer.CreateDefaultSubobject<UStaticMeshComponent>(this, TEXT("LeftMesh"));
+	rightMesh = ObjectInitializer.CreateDefaultSubobject<UStaticMeshComponent>(this, TEXT("RightMesh"));
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	if (GEngine->HMDDevice.IsValid() && GEngine->HMDDevice->IsHMDConnected()) // Check if we are in VR mode
+	{
+		leftHand = LeftMotionController;
+		rightHand = RightMotionController;
+
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("HMD is valid"));
+	}
+	else // If not, assign placeholder hand objects
+	{
+		auto lh = NewObject<UHandComponent>();
+		lh->SetControllerHand(EControllerHand::Left);
+		leftHand = lh;
+		 
+		auto rh = NewObject<UHandComponent>();
+		rh->SetControllerHand(EControllerHand::Right);
+		rightHand = rh;
+
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("HMD is not valid"));
+
+		FAttachmentTransformRules handRules(EAttachmentRule::KeepRelative, false);
+
+		leftHand->AttachToComponent(RootComponent, handRules);
+		rightHand->AttachToComponent(RootComponent, handRules);
+	}	
+
+	FAttachmentTransformRules meshRules(EAttachmentRule::SnapToTarget, false);
+	
+	leftMesh->AttachToComponent(leftHand, meshRules);
+	rightMesh->AttachToComponent(rightHand, meshRules);
+	
+	leftHand->RegisterComponent();
+	rightHand->RegisterComponent();
+}
+
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
 }
 
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* playerInputComponent)
 {
 	Super::SetupPlayerInputComponent(playerInputComponent);
 
-	//playerInputComponent->BindAxis("Turn", this, &APlayerCharacter::AddControllerYawInput);
-	//playerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::AddControllerPitchInput);
+	playerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+	playerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+
+	playerInputComponent->BindAxis("Turn", this, &APlayerCharacter::AddControllerYawInput);
+	playerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::AddControllerPitchInput);
 }
 
+
+void APlayerCharacter::MoveForward(float value)
+{
+	FVector direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(direction, value);
+}
+
+void APlayerCharacter::MoveRight(float value)
+{
+	FVector direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(direction, value);
+}
 
