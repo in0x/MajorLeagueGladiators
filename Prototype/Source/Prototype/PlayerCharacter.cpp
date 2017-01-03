ac303af20b459380a6aded38b632cf3b9bd18cac
Philipp Welsch <pwelsch86@gmail.com>
2017-01-03 10:36:51 +0100

    controllers now overlap all other objects

--- Prototype/Source/Prototype/PlayerCharacter.cpp
+++ Prototype/Source/Prototype/PlayerCharacter.cpp
@@ -25,6 +25,9 @@ APlayerCharacter::APlayerCharacter(const FObjectInitializer& ObjectInitializer /
 	leftGrabSphere->SetupAttachment(LeftMotionController);
 	rightGrabSphere->SetupAttachment(RightMotionController);
 
+	leftGrabSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	rightGrabSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
 	// ArrowComponent is used to determine position and direction for teleport.
 	//vrArrow = CastChecked<UArrowComponent>(GetComponentByClass(UArrowComponent::StaticClass())); 
 
