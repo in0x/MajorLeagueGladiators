48e41a785fce93fe54e33f762a4242235deefef9
Philipp Welsch <pwelsch86@gmail.com>
2017-01-12 17:31:12 +0100

    add damage caused property to DamageCauser

--- Prototype/Source/Prototype/DamageCauserComponent.cpp
+++ Prototype/Source/Prototype/DamageCauserComponent.cpp
@@ -16,9 +16,6 @@ void UDamageCauserComponent::BeginPlay()
 
 void UDamageCauserComponent::OnHit(AActor* selfActor, AActor* otherActor, FVector normalImpulse, const FHitResult& hit)
 {
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Emerald, TEXT("HIT"));
-
-	auto gunDamageType = TSubclassOf<UDamageType>(UDamageType::StaticClass());
-	
-	UGameplayStatics::ApplyPointDamage(otherActor, 10.f, normalImpulse, hit, nullptr, selfActor, gunDamageType);
+	auto gunDamageType = TSubclassOf<UDamageType>(UDamageType::StaticClass());	
+	UGameplayStatics::ApplyPointDamage(otherActor, damageAppliedOnHit, normalImpulse, hit, nullptr, selfActor, gunDamageType);
 }
