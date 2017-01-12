9eed8d03bb76d0b610855e7a73c33108b73dddf2
Philipp Welsch <pwelsch86@gmail.com>
2017-01-12 17:30:40 +0100

    rework health component

--- Prototype/Source/Prototype/HealthComponent.cpp
+++ Prototype/Source/Prototype/HealthComponent.cpp
@@ -6,6 +6,30 @@
 UHealthComponent::UHealthComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
+	RefillHealth();
 }
+
+float UHealthComponent::CurrentHealth() const
+{
+	return currentHealth;
+}
+
+float UHealthComponent::MaxHealth() const
+{
+	return maxHealth;
+}
+
+void UHealthComponent::DecreaseHealth(float val)
+{
+	val = std::abs(val);
+	currentHealth -= val;
+	currentHealth = std::max(0.f, currentHealth);
+}
+
+void UHealthComponent::RefillHealth()
+{
+	currentHealth = maxHealth;
+}
+
 
 
