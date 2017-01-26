652e12e225c05af0c0c4169bb9f6cb81d143f10d
Philipp Welsch <pwelsch86@gmail.com>
2017-01-26 11:50:20 +0100

    despawn gun projectile on collision

--- Prototype/Source/Prototype/GunProjectile.h
+++ Prototype/Source/Prototype/GunProjectile.h
@@ -15,12 +15,16 @@ class PROTOTYPE_API AGunProjectile : public AStaticMeshActor
 	
 public:
 	AGunProjectile();
+	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTimeS) override;
 	
 private:
 	// Time until this projectile is destroyed in seconds.
 	UPROPERTY(EditAnywhere)
 	float lifeTimeS;
+
+	UFUNCTION()
+	void OnHit(AActor* SelfActor, AActor* OtherActor, FVector NormalImpulse, const FHitResult& Hit);
 
 	float timeAliveS;
 };
