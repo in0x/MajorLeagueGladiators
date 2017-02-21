2d91174f3dc4acbb17e9ce3e0b5ed21b1c201adc
Florian Schmiderer <florian.schmiderer@yahoo.de>
2017-02-21 14:13:39 +0100

    reflect code for shield

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/ShieldActor.cpp
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/ShieldActor.cpp
@@ -3,7 +3,58 @@
 #include "MajorLeagueGladiator.h"
 #include "ShieldActor.h"
 
+#include "GunProjectile.h"
+
+namespace
+{
+	const char* REFLECT_SOCKET_NAME = "Reflect";
+}
+
 AShieldActor::AShieldActor()
 {
 	bReplicates = true;
+	GetStaticMeshComponent()->bGenerateOverlapEvents = true;
+}
+
+void AShieldActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	if (IsLocallyControlled() && IsReflectableProjectile(OtherActor))
+	{
+		FTransform socketTransform = GetStaticMeshComponent()->GetSocketTransform(REFLECT_SOCKET_NAME);
+
+		ReflectProjectile_Server(OtherActor, socketTransform.GetLocation(), socketTransform.Rotator());
+	}
+
+}
+
+bool AShieldActor::IsLocallyControlled() const
+{
+	AActor* owner = GetOwner();
+	APawn* ownerPawn = Cast<APawn>(owner);
+	if (ownerPawn)
+	{
+		return ownerPawn->IsLocallyControlled();
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Shieldactor was spawned without owner, reflection is disabled now"));
+	return false;
+}
+bool AShieldActor::IsReflectableProjectile(AActor* Projectile) const
+{
+	return Projectile->IsA<AGunProjectile>();
+}
+
+bool AShieldActor::ReflectProjectile_Server_Validate(AActor* Projectile, FVector SocketLocation, FRotator SocketRotation)
+{
+	return true;
+}
+
+void AShieldActor::ReflectProjectile_Server_Implementation(AActor* Projectile, FVector SocketLocation, FRotator SocketRotation)
+{
+	AActor* spawnedActor = GetWorld()->SpawnActor<AActor>(Projectile->GetClass(), SocketLocation, SocketRotation);
+	UPrimitiveComponent* rootComponent = CastChecked<UPrimitiveComponent>(spawnedActor->GetRootComponent());
+	rootComponent->AddImpulse(1000 * SocketRotation.Vector(), NAME_None, true);
+
+	Projectile->Destroy();
 }
