404c2fe8c38b424562dd7d8de6f4853c01ff2624
Florian Schmiderer <fschmiderer.mmt-b2014@fh-salzburg.ac.at>
2017-02-23 22:11:11 +0100

    code cleaning

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/Singleton.cpp
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/Singleton.cpp
@@ -3,12 +3,6 @@
 #include "MajorLeagueGladiator.h"
 #include "Singleton.h"
 
-
-namespace
-{
-	const char* REFLECT_SOCKET_NAME = "Reflect";
-}
-
 USingleton::USingleton(const FObjectInitializer& ObjectInitializer)
 {
 }
