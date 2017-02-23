a88235c36ed09b477be0f37e7ae7b89fe6c0bab0
Lucas Schiechl <lschiechl.mmt-b2014@fh-salzburg.ac.at>
2017-02-23 11:37:33 +0100

    changed DamageCauser, now SwordDamageCauser is separated more. once per slice damage is now done to all overlapping actors

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/DamageCauserComponent.cpp
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/DamageCauserComponent.cpp
@@ -2,8 +2,7 @@
 
 #include "MajorLeagueGladiator.h"
 #include "DamageCauserComponent.h"
-#include "MlgGrippableStaticMeshActor.h"
-#include "MlgPlayerController.h"
+
 
 UDamageCauserComponent::UDamageCauserComponent()
 	: damageAppliedOnHit(0)
@@ -19,36 +18,5 @@ void UDamageCauserComponent::BeginPlay()
 
 void UDamageCauserComponent::OnBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
 {
-	if (CanDoDamage())
-	{
-		UGameplayStatics::ApplyDamage(OtherActor, damageAppliedOnHit, nullptr, OverlappedActor, damageType);
-		doRumbleRight(OtherActor);
-	}
-}
-
-bool UDamageCauserComponent::CanDoDamage()
-{
-	return true;
-}
-
-void UDamageCauserComponent::doRumbleRight(AActor* OtherActor)
-{
-	AMlgGrippableStaticMeshActor* owner = Cast<AMlgGrippableStaticMeshActor>(GetOwner());
-	if (owner != nullptr)
-	{
-		AMlgPlayerController* controller = owner->GetMlgPlayerController();
-		if (controller != nullptr)
-		{
-			//check if hit object is part of myself, then we do not rumble
-			APawn* otherPlayer = Cast<APawn>(OtherActor);
-			if (otherPlayer != nullptr)
-			{
-				if (otherPlayer->Controller == controller)
-				{
-					return;
-				}
-			}
-			controller->ClientPlayForceFeedback(controller->GetRumbleShortRight(), false, FName("rumbleRight"));
-		}
-	}
+	UGameplayStatics::ApplyDamage(OtherActor, damageAppliedOnHit, nullptr, OverlappedActor, damageType);
 }
