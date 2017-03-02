da8461794bac4aa401a1f045d649726b121903df
Philipp Welsch <pwelsch86@gmail.com>
2017-03-02 16:38:13 +0100

    implement component

---
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/RespawnComponent.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MajorLeagueGladiator.h"
+#include "RespawnComponent.h"
+
+URespawnComponent::URespawnComponent()
+{
+	bGenerateOverlapEvents = true;
+	bMultiBodyOverlap = true;
+	SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	OnComponentBeginOverlap.AddDynamic(this, &URespawnComponent::OnOverlapBegin);
+}
+
+void URespawnComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	auto mesh = GetStaticMesh();
+	checkf(mesh, TEXT("RespawnComponent requires a mesh"));
+}
+
+void URespawnComponent::OnOverlapBegin(UPrimitiveComponent* SelfComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIdx, bool bFromSweep, const FHitResult& Hit)
+{
+	Other->TeleportTo(respawnPosition, Other->GetActorRotation());
+}
+
