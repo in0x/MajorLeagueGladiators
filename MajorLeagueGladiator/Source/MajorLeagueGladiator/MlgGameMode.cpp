b87333848ef94f2ed6e86410187c25b0a427d8d5
Florian Schmiderer <florian.schmiderer@yahoo.de>
2017-03-13 23:06:28 +0100

    different classes can be spawned on client and server

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/MlgGameMode.cpp
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/MlgGameMode.cpp
@@ -12,3 +12,33 @@ AMlgGameMode::AMlgGameMode(const FObjectInitializer& ObjectInitializer)
 	//PlayerControllerClass = AMlgPlayerController::StaticClass();
 	PlayerStateClass = AMlgPlayerState::StaticClass();
 }
+
+UClass* AMlgGameMode::GetDefaultPawnClassForController_Implementation(AController* InController)
+{
+	// Handle only Player Controllers for now, as AI is spawned differently
+	check(InController->IsPlayerController());
+
+	// For now this just assigns the Server the DPS class and the Client the Tank Class.
+	// Game Mode is only on the Server. If the controller is locally controlled it is the
+	// server's controller
+	if (InController->IsLocalPlayerController())
+	{
+		if (dpsClass.Get() == nullptr)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("DPS Class not set. Using Default Pawn Class"));
+			return Super::GetDefaultPawnClassForController_Implementation(InController);
+		}
+
+		return dpsClass.Get();
+	}
+	else
+	{
+		if (tankClass.Get() == nullptr)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Tank Class not set. Using Default Pawn Class"));
+			return Super::GetDefaultPawnClassForController_Implementation(InController);
+		}
+
+		return tankClass.Get();
+	}
+}
