4b3436209b5710af02dd14de53d54f766063b778
Florian Schmiderer <florian.schmiderer@yahoo.de>
2017-03-14 13:33:51 +0100

    added the 2 player classes

---
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/Characters/MeleePlayerCharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "MlgPlayerCharacter.h"
+#include "MeleePlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAJORLEAGUEGLADIATOR_API AMeleePlayerCharacter : public AMlgPlayerCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
