2c4c715c64110520f490b6778dfc232d3f3a92c1
Florian Schmiderer <florian.schmiderer@yahoo.de>
2017-03-14 13:33:51 +0100

    added the 2 player classes

---
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/Characters/RangedPlayerCharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "MlgPlayerCharacter.h"
+#include "RangedPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAJORLEAGUEGLADIATOR_API ARangedPlayerCharacter : public AMlgPlayerCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
