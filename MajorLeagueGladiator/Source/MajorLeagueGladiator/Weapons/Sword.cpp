a1c597ea8d3b0fb3f574d75fefed67b3a3ccd916
Florian Schmiderer <florian.schmiderer@yahoo.de>
2017-03-21 19:56:51 +0100

    added sword collison channel

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/Weapons/Sword.cpp
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/Weapons/Sword.cpp
@@ -3,8 +3,14 @@
 #include "MajorLeagueGladiator.h"
 #include "Sword.h"
 
+namespace
+{
+	const char* MELEE_WEAPON_COLLISION_PROFILE_NAME = "MeleeWeapon";
+}
+
 ASword::ASword(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 	GetStaticMeshComponent()->bGenerateOverlapEvents = true;
+	GetStaticMeshComponent()->SetCollisionProfileName(MELEE_WEAPON_COLLISION_PROFILE_NAME);
 }
