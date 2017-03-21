f19219b056b01e7ea327f69378bb67188355ae29
Florian Schmiderer <florian.schmiderer@yahoo.de>
2017-03-21 19:29:02 +0100

    collision fixing, added ranged weapon collsion setting

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/Weapons/MlgWeapon.cpp
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/Weapons/MlgWeapon.cpp
@@ -3,6 +3,17 @@
 #include "MajorLeagueGladiator.h"
 #include "MlgWeapon.h"
 
+namespace 
+{
+	const char* RANGED_WEAPON_COLLISION_PROFILE_NAME = "RangedWeapon";
+}
 
+AMlgWeapon::AMlgWeapon(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	UPrimitiveComponent* rootPrimitve = CastChecked<UPrimitiveComponent>(RootComponent);
+	rootPrimitve->SetSimulatePhysics(false);
+	rootPrimitve->SetCollisionProfileName(RANGED_WEAPON_COLLISION_PROFILE_NAME);
+}
 
 
