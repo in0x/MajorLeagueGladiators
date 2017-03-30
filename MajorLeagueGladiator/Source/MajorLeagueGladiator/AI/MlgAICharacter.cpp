80f7c904a5eae7babbf263211a7c0a3e96518ef1
Philipp Welsch <pwelsch86@gmail.com>
2017-03-30 13:56:42 +0200

    rewrite damage vis to work with weakpoints

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/AI/MlgAICharacter.cpp
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/AI/MlgAICharacter.cpp
@@ -36,6 +36,18 @@ AMlgAICharacter::AMlgAICharacter(const FObjectInitializer& ObjectInitializer)
 float AMlgAICharacter::InternalTakePointDamage(float Damage, const FPointDamageEvent& PointDamageEvent, AController* EventInstigator, AActor* DamageCauser)
 {
 	FWeakpoint hitWeakpoint = weakpoints->FindHitWeakpoint(PointDamageEvent.HitInfo);
+
+	UMeshComponent* mesh = FindComponentByClass<UMeshComponent>();
+
+	if (hitWeakpoint.LocationSocketName != NAME_None)
+	{
+		damageVisualizer->AddVisual_NetMulticast(mesh, true, FTransform(PointDamageEvent.HitInfo.ImpactPoint));
+	}
+	else
+	{
+		damageVisualizer->AddVisual_NetMulticast(mesh, false);
+	}
+
 	return Damage * hitWeakpoint.DamageMultiplier;
 }
 
