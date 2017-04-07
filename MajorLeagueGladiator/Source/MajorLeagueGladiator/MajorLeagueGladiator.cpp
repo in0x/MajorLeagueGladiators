660bb0abcf40cb7f5a9927ddc5cfffa007db5c31
MajorLeagueGladiator <majorleaguegladiators@gmail.com>
2017-04-07 15:46:26 +0200

    fixed g_IsVrEnabled

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/MajorLeagueGladiator.cpp
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/MajorLeagueGladiator.cpp
@@ -7,6 +7,6 @@ DEFINE_LOG_CATEGORY(DebugLog);
 
 bool g_IsVREnabled()
 {
-	bool isEnabled = FParse::Param(FCommandLine::Get(), TEXT("vr")) || (GEngine && GEngine->HMDDevice.IsValid() && GEngine->HMDDevice->IsHMDConnected());
+	bool isEnabled = FParse::Param(FCommandLine::Get(), TEXT("vr")) && (GEngine && GEngine->HMDDevice.IsValid() && GEngine->HMDDevice->IsHMDConnected());
 	return isEnabled;
 }
