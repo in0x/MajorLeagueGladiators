aec5e9d5f6c9cd830be91a08b0423f813c6496ee
Alessandro Morio <amorio.mmt-b2014@fh-salzburg.ac.at>
2017-04-12 13:54:14 +0200

    basic working melee/damagefeedback component used in AICharacter

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/DamageFeedbackComponent.h
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/DamageFeedbackComponent.h
@@ -5,22 +5,59 @@
 #include "Components/ActorComponent.h"
 #include "DamageFeedbackComponent.generated.h"
 
+USTRUCT()
+struct FDamageFeedbackData
+{
+	GENERATED_USTRUCT_BODY()
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+		UPROPERTY(EditAnywhere, Category = "DamageVisual")
+		TArray<UParticleSystemComponent*> ParticleSystems;
+
+	UPROPERTY(EditAnywhere, Category = "DamageVisual")
+		UMeshComponent* AffectedMesh;
+
+	UPROPERTY(EditAnywhere, Category = "DamageVisual")
+		UMaterialInstanceDynamic* MatInstance;
+
+	UPROPERTY(EditAnywhere, Category = "DamageVisual")
+		FName DamageValParamName;
+
+	UPROPERTY(EditAnywhere, Category = "DamageVisual")
+		float MatVisDuration;
+
+	UPROPERTY(EditAnywhere, Category = "DamageVisual")
+		float CurrentMatVisDuration;
+
+	FDamageFeedbackData()
+		: AffectedMesh(nullptr)
+		, MatInstance(nullptr)
+		, DamageValParamName(FName("DamageValue"))
+		, MatVisDuration(0.3f)
+		, CurrentMatVisDuration(0.f)
+	{}
+};
+
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class MAJORLEAGUEGLADIATOR_API UDamageFeedbackComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
+
 	UDamageFeedbackComponent();
 
-public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UFUNCTION(NetMulticast, reliable)
-	virtual void AddVisual_NetMulticast(UMeshComponent* affectedMesh, bool bSpawnParticles, const FTransform& particleTrafo = FTransform(), const UDamageType* DamageType = nullptr) = 0;
+	virtual void AddVisual_NetMulticast(UMeshComponent* affectedMesh, bool bSpawnParticles, const FPointDamageEvent& PointDamageEvent = FPointDamageEvent(), const UDamageType* DamageType = nullptr);
 		
-	
+protected:
+
+	TArray<FDamageFeedbackData> feedbackData;
+
+	UPROPERTY(EditAnywhere, Category = "Feedback")
+	TArray<UParticleSystem*> particleSystems;
+
+	UPROPERTY(EditAnywhere, Category = "Feedback")
+	TArray<USoundBase*> sounds;
 };
---
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/MeleeDamageFeedbackComponent.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "DamageFeedbackComponent.h"
+#include "MeleeDamageFeedbackComponent.generated.h"
+
+UCLASS(Blueprintable, meta = (BlueprintSpawnableComponent))
+class MAJORLEAGUEGLADIATOR_API UMeleeDamageFeedbackComponent : public UDamageFeedbackComponent
+{
+	GENERATED_BODY()
+	
+public:
+
+	UMeleeDamageFeedbackComponent();
+/*
+	UFUNCTION(NetMulticast, reliable)
+	virtual void AddVisual_NetMulticast(UMeshComponent* affectedMesh, bool bSpawnParticles, const FPointDamageEvent& PointDamageEvent = FPointDamageEvent(), const UDamageType* DamageType = nullptr) override;
+	*/
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Melee Feedback")
+	UParticleSystem* weakpointParticleSystem;
+	
+};
