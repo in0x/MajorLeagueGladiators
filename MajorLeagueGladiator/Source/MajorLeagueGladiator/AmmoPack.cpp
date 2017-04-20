0ee1b7ef4778a1ba59ef47fa15b0eed9cafdcd6d
MajorLeagueGladiator <majorleaguegladiators@gmail.com>
2017-04-20 13:06:14 +0200

    hardcoded BP values into blueprint

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/AmmoPack.cpp
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/AmmoPack.cpp
@@ -5,12 +5,31 @@
 #include "TriggerZoneComponent.h"
 #include "AmmoComponent.h"
 
+namespace
+{
+	const char* PACK_COLLISION_PROFILE_NAME = "Pack";
+}
+
 AAmmoPack::AAmmoPack()
 {
 	SetReplicates(true);
 
 	MeshComponent->bGenerateOverlapEvents = true;
 	MeshComponent->bMultiBodyOverlap = true;
+	MeshComponent->SetRelativeScale3D({ 0.25,0.25,0.25 });
+	MeshComponent->SetSimulatePhysics(true);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> ammoPackMesh(TEXT("StaticMesh'/Game/MobileStarterContent/Shapes/Shape_Cube.Shape_Cube'"));
+	UStaticMeshComponent* staticMeshComp = Cast<UStaticMeshComponent>(MeshComponent);
+	if (ammoPackMesh.Succeeded() && staticMeshComp)
+	{
+		staticMeshComp->SetStaticMesh(ammoPackMesh.Object);
+
+		ConstructorHelpers::FObjectFinder<UMaterialInterface> ammoPackMat(TEXT("Material'/Game/BluePrints/AmmoPackMat.AmmoPackMat'"));
+		staticMeshComp->SetMaterial(0, ammoPackMat.Object);
+	}
+	bReplicateMovement = true;
+	MeshComponent->SetCollisionProfileName(PACK_COLLISION_PROFILE_NAME);
 }
 
 void AAmmoPack::Use(AActor* User, TriggerType Type)
