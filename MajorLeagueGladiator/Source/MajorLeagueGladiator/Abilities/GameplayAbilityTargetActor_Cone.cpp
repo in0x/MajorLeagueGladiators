d41c20c281685030230c9c4664f8fa9715dc6b0e
MajorLeagueGladiator <majorleaguegladiators@gmail.com>
2017-04-28 10:45:32 +0200

    refactores coneTargeting, add visualizer

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/Abilities/GameplayAbilityTargetActor_Cone.cpp
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/Abilities/GameplayAbilityTargetActor_Cone.cpp
@@ -1,6 +1,46 @@
 #include "MajorLeagueGladiator.h"
 #include "GameplayAbilityTargetActor_Cone.h"
 
+#include "TargetingSplineMeshComponent.h"
+
+
+AGameplayAbilityTargetActor_Cone::AGameplayAbilityTargetActor_Cone(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+	, Distance(200.0f)
+	, halfAngleDegrees(45.0f)
+	, halfAngleRadians(FMath::DegreesToRadians(45.0f))
+	, IsVisualizingCone(false)
+	, AutoConfirm(false)
+{
+	coneVisualizer = ObjectInitializer.CreateDefaultSubobject<UTargetingSplineMeshComponent>(this, TEXT("SplineMesh"));
+}
+
+
+void AGameplayAbilityTargetActor_Cone::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	TArray<TWeakObjectPtr<AActor>> foundActors = Trace();
+
+	if (foundActors.Num() > 0 && AutoConfirm)
+	{
+		TargetDataReadyDelegate.Broadcast(MakeTargetHandle(foundActors));
+	}
+}
+
+
+void AGameplayAbilityTargetActor_Cone::StartTargeting(UGameplayAbility* Ability)
+{
+	Super::StartTargeting(Ability);
+	SetActorHiddenInGame(IsVisualizingCone);
+	if (IsVisualizingCone)
+	{
+		const float endScale = CalcEndConeScale();
+	
+		coneVisualizer->SetEndScale({ endScale, endScale });
+	}
+}
+
 
 void AGameplayAbilityTargetActor_Cone::ConfirmTargetingAndContinue()
 {
@@ -26,6 +66,7 @@ TArray<TWeakObjectPtr<AActor>> AGameplayAbilityTargetActor_Cone::Trace() const
 	const FVector targetingLocation = targetingTransform.GetLocation();
 	const FQuat targetingRotation = targetingTransform.GetRotation();
 	const FVector targetingDirecton = targetingRotation.GetForwardVector();
+	const FVector targetingEnd = targetingLocation + targetingDirecton * Distance;
 
 	UWorld* world = GetWorld();
 	world->OverlapMultiByObjectType(
@@ -38,6 +79,7 @@ TArray<TWeakObjectPtr<AActor>> AGameplayAbilityTargetActor_Cone::Trace() const
 		);
 
 	TArray<TWeakObjectPtr<AActor>> foundActors;
+
 	for (auto item : outOverlaps)
 	{
 		if (ACharacter* character = Cast<ACharacter>(item.Actor.Get()))
@@ -48,17 +90,36 @@ TArray<TWeakObjectPtr<AActor>> AGameplayAbilityTargetActor_Cone::Trace() const
 			const float angleRadiens = acosf(FVector::DotProduct(normalizedDistance, targetingDirecton));
 			const float angleDegrees = FMath::RadiansToDegrees(angleRadiens);
 
-			if (angleDegrees < HalfAngleDegrees)
+			if (angleDegrees < halfAngleDegrees)
 			{
 				foundActors.Add(character);
 			}
 		}
 	}
 
+	if (IsVisualizingCone)
+	{
+		coneVisualizer->SetFromRayCast(targetingLocation, targetingEnd, foundActors.Num() > 0);
+	}
+
 	return foundActors;
 }
+
+void AGameplayAbilityTargetActor_Cone::SetHalfAngleDegrees(float NewHalfAngleInDegrees)
+{
+	halfAngleDegrees = NewHalfAngleInDegrees;
+	halfAngleRadians = FMath::DegreesToRadians(NewHalfAngleInDegrees);
+}
+
 
 FGameplayAbilityTargetDataHandle AGameplayAbilityTargetActor_Cone::MakeTargetHandle(const TArray<TWeakObjectPtr<AActor>> Actors) const
 {
 	return StartLocation.MakeTargetDataHandleFromActors(Actors);
 }
+
+
+float AGameplayAbilityTargetActor_Cone::CalcEndConeScale() const
+{
+	return Distance * FMath::Sin(halfAngleRadians);
+}
+
