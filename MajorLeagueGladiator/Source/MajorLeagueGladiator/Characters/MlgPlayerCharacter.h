138715a4e3d50863b3156131d7945646153fdda3
Philipp Welsch <pwelsch86@gmail.com>
2017-05-18 16:58:31 +0200

    initial implementation

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/Characters/MlgPlayerCharacter.h
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/Characters/MlgPlayerCharacter.h
@@ -41,6 +41,7 @@ public:
 	void MoveRight(float Value);
 	void SetLeftTriggerStatus(float Value);
 	void SetRightTriggerStatus(float Value);
+	void OnSideGripAxisRight(float Value);
 
 	void OnLeftTriggerClicked();
 	void OnLeftTriggerReleased();
@@ -164,6 +165,9 @@ private:
 
 	UFUNCTION(Server, WithValidation, reliable)
 	void rightHandDrop_Server();
+
+	UFUNCTION(Server, WithValidation, reliable)
+	void directionalMove_Server(float Value);
 
 	void SpawnWeapon();
 
