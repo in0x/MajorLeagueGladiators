19701062a02e83b1458ec2552fa137dcb8fbe580
MajorLeagueGladiator <majorleaguegladiators@gmail.com>
2017-05-26 14:07:28 +0200

    fixed shield collison

--- MajorLeagueGladiator/Source/MajorLeagueGladiator/ShieldActor.cpp
+++ MajorLeagueGladiator/Source/MajorLeagueGladiator/ShieldActor.cpp
@@ -7,21 +7,21 @@
 #include "MlgPlayerController.h"
 #include "MlgGameplayStatics.h"
 #include "Components/PoseableMeshComponent.h"
+#include "Animation/BlendSpace1D.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Animation/AnimSingleNodeInstance.h"
 
 namespace
 {
 	const FName REFLECT_SOCKET_NAME("Reflect");
 	const FName SHIELD_COLLISION_PRESET_NAME("Shield");
-	const FName CENTER_BONE_NAME("middle_JT");
 }
 
 AShieldActor::AShieldActor(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer.SetDefaultSubobjectClass<UPoseableMeshComponent>(Super::MESH_COMPONENT_NAME))
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<USkeletalMeshComponent>(Super::MESH_COMPONENT_NAME))
 	, startActiveTime(0)
 	, currentActiveTime(0)
-	, maxShieldSeconds(3.f)
-	, centerMaxScale(5.f)
-	, centerMinScale(0.f)
+	, maxShieldSeconds(3.f) 
 {
 	PrimaryActorTick.bCanEverTick = true;
 	PrimaryActorTick.bStartWithTickEnabled = true;
@@ -29,10 +29,12 @@ AShieldActor::AShieldActor(const FObjectInitializer& ObjectInitializer)
 	MeshComponent->bGenerateOverlapEvents = true;
 	MeshComponent->SetCollisionProfileName(SHIELD_COLLISION_PRESET_NAME);
 
-	UPoseableMeshComponent* skeletalMeshComp = CastChecked<UPoseableMeshComponent>(MeshComponent);
+	USkeletalMeshComponent* skeletalMeshComp = CastChecked<USkeletalMeshComponent>(MeshComponent);
 
 	ConstructorHelpers::FObjectFinder<USkeletalMesh> skeletalMesh(TEXT("SkeletalMesh'/Game/MVRCFPS_Assets/Shield/shield.shield'"));
 	skeletalMeshComp->SetSkeletalMesh(skeletalMesh.Object);
+	ConstructorHelpers::FObjectFinder<UBlendSpace1D> blendAnim(TEXT("BlendSpace1D'/Game/MVRCFPS_Assets/Shield/ShieldCharge.ShieldCharge'"));
+	skeletalMeshComp->OverrideAnimationData(blendAnim.Object);
 
 	ConstructorHelpers::FObjectFinder<USoundCue> spawnSoundCueFinder(TEXT("SoundCue'/Game/MVRCFPS_Assets/Sounds/ShieldSpawnSound_Cue.ShieldSpawnSound_Cue'"));
 	spawnSoundCue = spawnSoundCueFinder.Object;
@@ -96,11 +98,11 @@ void AShieldActor::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLife
 void AShieldActor::updateAnimation(float timeRemaining)
 {
 	const float percentLeft = timeRemaining / maxShieldSeconds;
-	const float scale = (centerMaxScale - centerMinScale) * percentLeft + centerMinScale;
 
-	const FVector scaleVector(1, scale, scale);
-	UPoseableMeshComponent* shieldMesh = CastChecked<UPoseableMeshComponent>(MeshComponent);
-	shieldMesh->SetBoneScaleByName(CENTER_BONE_NAME, scaleVector, EBoneSpaces::ComponentSpace);
+	FVector BlendParams(percentLeft, 0.0f, 0.0f);
+	
+	USkeletalMeshComponent* shieldMesh = CastChecked<USkeletalMeshComponent>(MeshComponent);
+	shieldMesh->GetSingleNodeInstance()->SetBlendSpaceInput(BlendParams);
 }
 
 void AShieldActor::playSpawnSound()
@@ -111,7 +113,7 @@ void AShieldActor::playSpawnSound()
 void AShieldActor::OnHitInteractable(const ABaseProjectile* projectile)
 {
 	FTransform socketTransform = GetReflectSpawnTransform();
-	projectile->FireProjectile(socketTransform.GetLocation(), socketTransform.Rotator().Vector(), this, Instigator->GetController());
+	projectile->FireProjectile(socketTransform.GetLocation(), socketTransform.GetRotation().GetForwardVector(), this, Instigator->GetController());
 	PlayReflectSound();
 
 	if (AMlgPlayerController* playerController = GetMlgPlayerController())
